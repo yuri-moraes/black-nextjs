5af83d89840565f5cdef0d8777315f25c44aa3ee
Yuri Nogueira de Moraes <yuri.nogueira.de.moraes@hotmail.com>
2024-08-20 15:06:14 -0300

    adicionado a página produtos

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,7 +1,20 @@
-import { NextPage } from "next";
+import { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
+import { Container } from "reactstrap";
+import Header from "@/components/Header";
+import ProductsList from "@/components/ProductsList";
+import { fetchProducts, ProductType } from "@/services/products";
 
-const Products: NextPage = () => {
+export const getStaticProps: GetStaticProps = async () => {
+  const products = await fetchProducts();
+  return { props: { products } };
+};
+
+type ProductsPageProps = {
+  products?: ProductType[];
+};
+
+const Products: NextPage<ProductsPageProps> = ({ products }) => {
   return (
     <>
       <Head>
@@ -10,7 +23,18 @@ const Products: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <h1>Nossos Produtos</h1>
+      <Header />
+
+      <main>
+        <Container className="mb-5">
+          <h1 className="my-5">Nossos Produtos</h1>
+          {products ? (
+            <ProductsList products={products} />
+          ) : (
+            <p>Nenhum produto encontrado.</p>
+          )}
+        </Container>
+      </main>
     </>
   );
 };
